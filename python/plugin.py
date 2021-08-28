b427bddf5aca245682ae00bcbc1adc1d938add78
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-08-28 12:16:07 -0700

    Update plugin.py

--- python/plugin.py
+++ python/plugin.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+# imports
 import os
 import sys
+# import the drum api
 home = os.path.expanduser('~')
 mymodule_dir = os.path.join( home, 'drumbash', 'api')
 sys.path.append( mymodule_dir )
 import api
+
+# replace the code below with your own code
 print(api.drumver())
